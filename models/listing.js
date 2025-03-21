470fa6eb8e5ac92c1d92d32b34f97c0a038d38b9
kamlesh <kamleshg9569@gmail.com>
2025-03-22 00:20:56 +0530

    adding style

--- models/listing.js
+++ models/listing.js
@@ -12,11 +12,11 @@ const listingSchema = new Schema({
     }, 
     image: {
         type: String,
-        default: 
-            "https://unsplash.com/photos/silhouette-photo-of-man-on-cliff-during-sunset-_6HzPU9Hyfg",
+        default: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        
         set: (v) => 
             v === ""
-                ? "https://unsplash.com/photos/silhouette-photo-of-man-on-cliff-during-sunset-_6HzPU9Hyfg"
+                ? "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
                 : v,
         
     },
