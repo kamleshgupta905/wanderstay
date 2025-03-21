a59586b97072863dbe0114f71244fbe0e3848492
kamlesh <kamleshg9569@gmail.com>
2025-03-21 16:22:23 +0530

    changes something in index.js

--- init/index.js
+++ init/index.js
@@ -7,7 +7,9 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
     .then(() => {
+
         console.log("connected to DB");
+        
     })
     .catch((err) => {
         console.log(err);
